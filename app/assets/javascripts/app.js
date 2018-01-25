cfba24500aa3004b7a7e24f8611a802df91b9099
Ges Jeremie <ges.jeremie@gmail.com>
2018-01-25 07:28:58 -0800

    Use strict component select

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,14 +2,7 @@
     'use strict';
 
     new Vue({
-        el: '#app',
-        data: {
-          options: [
-            'foo',
-            'bar',
-            'baz'
-          ]
-        }
+        el: '#app'
     });
 
 }());
--- app/assets/javascripts/components/component-select.js
+++ app/assets/javascripts/components/component-select.js
@@ -1,4 +1,5 @@
 (function() {
+    'use strict';
 
     Vue.component('component-select', VueSelect.VueSelect);
 
