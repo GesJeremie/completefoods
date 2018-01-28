f0c2994fcc9fc642c0e5eda084cb8961ed2117b4
Ges Jeremie <ges.jeremie@gmail.com>
2018-01-28 09:07:41 -0800

    Improve display decimals for low prices

--- app/assets/javascripts/filters/formatCurrency.js
+++ app/assets/javascripts/filters/formatCurrency.js
@@ -2,6 +2,12 @@
     'use strict';
 
     Vue.filter('formatCurrency', function(value) {
+        value = parseFloat(value);
+
+        if (value < 1) {
+            return numeral(value).format('0,0.000');
+        }
+
         return numeral(value).format('0,0.00');
     });
 
