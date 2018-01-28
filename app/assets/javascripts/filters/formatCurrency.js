a9d854851a84b299b65b6d1558da09faa83409b3
Ges Jeremie <ges.jeremie@gmail.com>
2018-01-28 15:59:15 -0800

    Fix stuff

--- app/assets/javascripts/filters/formatCurrency.js
+++ app/assets/javascripts/filters/formatCurrency.js
@@ -4,7 +4,7 @@
     Vue.filter('formatCurrency', function(value) {
         value = parseFloat(value);
 
-        if (value < 1) {
+        if (value > 0 && value < 1) {
             return numeral(value).format('0,0.000');
         }
 
