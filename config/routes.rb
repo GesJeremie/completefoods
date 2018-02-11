d2aead8999cbcfe346506cfd528d3916dbeb385c
Ges Jeremie <ges.jeremie@gmail.com>
2018-02-11 23:02:00 +0100

    Add constraints redirect router

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  constraints subdomain: 'www' do
+    get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
+  end
+
   root 'home#index'
 
   resources :folio_crypto_currency,   only: [:create, :destroy, :update]
