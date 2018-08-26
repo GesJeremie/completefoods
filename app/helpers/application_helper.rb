b22c56bbff19925abe27a95a9689ad2cbd29e9c4
Jeremie Ges <jges@weblinc.com>
2018-08-26 15:19:45 +0300

    Add selected class for currency header + add header products show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,12 @@ module ApplicationHelper
   def current_currency
     Currency.find_by_code(session[:current_currency]) || Currency.find_by_code('USD')
   end
+
+  def currency_selected_class(currency, current_currency)
+    if currency.code == current_currency.code
+      'currencies__item-link currencies__item-link--selected'
+    else
+      'currencies__item-link'
+    end
+  end
 end
