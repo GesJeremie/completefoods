f18bf52a1da3d72f85095db9309152acb365f76a
Jeremie Ges <jges@weblinc.com>
2018-08-29 09:09:26 +0300

    Change TTL money.rb openexchanges

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -5,6 +5,6 @@ open_exchange = Money::Bank::OpenExchangeRatesBank.new
 open_exchange.app_id = 'abb47bbd898442878f65ba7b133ae713'
 
 open_exchange.update_rates
-open_exchange.ttl_in_seconds = 1.day.to_i
+open_exchange.ttl_in_seconds = 7.day.to_i
 
 Money.default_bank = open_exchange
