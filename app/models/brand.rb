233b307db17fcd01329622c1bd4ac2fcba4112ff
Jeremie Ges <jges@weblinc.com>
2018-08-31 06:54:54 +0300

    Add default scope order brand model

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -6,4 +6,6 @@ class Brand < ApplicationRecord
   validates :website, presence: true
   validates :facebook, presence: true
   validates :country_id, presence: true
+
+  default_scope { order(id: :asc) }
 end
