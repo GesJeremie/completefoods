b773db2e102691aaea1dd5e04415706ca9983da9
Jeremie Ges <jges@weblinc.com>
2018-09-20 23:54:08 -0700

    Gzip all the things

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module App
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
   end
 end
