424142fb9d67c0e70f51ead6b233a9f36ac02666
Jeremie Ges <jges@weblinc.com>
2018-10-05 20:31:07 -0700

    Change default order reviews, tweak scss

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -6,6 +6,8 @@ class ProductReview < ApplicationRecord
   validates :score, presence: true, inclusion: { in: (0..4).to_a }
   validates :ip, presence: true
 
+  default_scope { order(created_at: :desc) }
+
   def score_humanized
     SCORES[score]
   end
