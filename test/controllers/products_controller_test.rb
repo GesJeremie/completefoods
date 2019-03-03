15aa2e77ac798945bcd3bcb726ba983f9fc7e0e1
Jeremie Ges <jges@weblinc.com>
2019-03-03 15:15:07 -0800

    Add simple tests for products controller

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test 'display index' do
+    get products_path
+    assert_response :success
+  end
+
+  test 'display show' do
+    product = create(:product)
+    get products_path(product.slug)
+    assert_response :success
+  end
+end
