8e6598f464bc0c0586468e302f382e3ce5a467a9
Jeremie Ges <jges@weblinc.com>
2019-08-22 02:53:47 -0700

    Refaction application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :redirect_if_old_domain
+  helper_method :current_currency
+
+  OLD_DOMAINS = ['www.soylent-alternatives.com', 'soylent-alternatives.com'].freeze
 
   def current_currency
-    @current_currency ||= begin
-        Currency.find_by_code(session[:current_currency]) || Currency.find_by_code('USD')
-    end
+    @current_currency ||= Currency.find_by_code(session[:current_currency]) || Currency.find_by_code('USD')
   end
-  helper_method :current_currency
 
   def view_model_options
     params.to_unsafe_h.merge(
@@ -16,11 +16,20 @@ class ApplicationController < ActionController::Base
   end
 
   private
-    def redirect_if_old_domain
-      return unless Rails.env.production?
 
-      if ['www.soylent-alternatives.com', 'soylent-alternatives.com'].include?(request.host)
-        redirect_to "#{request.protocol}completefood.guru#{request.fullpath}", :status => :moved_permanently
-      end
+  def redirect_if_old_domain
+    return unless Rails.env.production?
+
+    if OLD_DOMAINS.include?(current_domain)
+      redirect_to new_domain, status: :moved_permanently
     end
+  end
+
+  def current_domain
+    request.host
+  end
+
+  def new_domain
+    "#{request.protocol}completefood.guru#{request.fullpath}"
+  end
 end
