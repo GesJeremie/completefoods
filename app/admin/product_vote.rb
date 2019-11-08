7085df9d2d4c8ee25fac5b386d6bb926c7bd0018
Jeremie Ges <jeremie@syatt.io>
2019-11-08 09:58:41 -0800

    Let the admin add new votes without limitation

--- app/admin/product_vote.rb
+++ app/admin/product_vote.rb
@@ -1,6 +1,27 @@
 ActiveAdmin.register ProductVote do
-  permit_params :product_id, :ip, :recommend, :reason, :active
+  permit_params :product_id, :ip, :recommend, :reason, :active, :admin
 
   scope :published
   scope :not_published
+
+  controller do
+    def create
+      params[:product_vote][:ip] = request.ip
+      params[:product_vote][:admin] = true
+      super
+    end
+  end
+
+  form do |f|
+    semantic_errors *f.object.errors.keys
+
+    inputs 'Attributes' do
+      f.input :product
+      f.input :recommend
+      f.input :reason, as: :text
+      f.input :active
+    end
+
+    f.actions
+  end
 end
--- app/models/product_vote.rb
+++ app/models/product_vote.rb
@@ -2,10 +2,11 @@ class ProductVote < ApplicationRecord
   belongs_to :product, touch: true
 
   attribute :active, :boolean, default: false
+  attribute :admin, :boolean, default: false
 
   validates :product, presence: true
   validates :ip, presence: true
-  validates_uniqueness_of :ip, { scope: :product, message: 'You already reviewed this product!' }
+  validates_uniqueness_of :ip, { scope: :product, message: 'You already reviewed this product!', unless: :admin? }
   validates :recommend, inclusion: BOOLEANS
   validates :reason, presence: true
   validates :active, inclusion: BOOLEANS
